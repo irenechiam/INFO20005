caf44c74bdaa6b9bde482bafb757a056493e3e98
CY <pchinyun@gmail.com>
2024-05-16 13:48:15 +1000

    added right moving to carousel

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const slider = document.querySelector('.slider'); 
 
-const leftArrow = document.querySelector('.left')
-const rightArrow = document.querySelector('.right')
-rightArrow.addEventListener('click',function())
+const leftArrow = document.querySelector('.arrow_left');
+const rightArrow = document.querySelector('#arrow_right');
+
+let sectionIndex = 0;
+
+document.addEventListener('click',function() { 
+    sectionIndex = sectionIndex + 1;
+    document.querySelector('.slider').style.transform = 'translate(' + (sectionIndex) * -33.3 + '%)';
+}); 
