65b2d0eaf3df737ebb19bb7e739c3c86fd31b342
CY <pchinyun@gmail.com>
2024-05-16 13:05:26 +1000

    changed container margin

---
+++ script.js
@@ -0,0 +1,5 @@
+const slider = document.querySelector('.slider'); 
+
+const leftArrow = document.querySelector('.left')
+const rightArrow = document.querySelector('.right')
+rightArrow.addEventListener('click',function())
