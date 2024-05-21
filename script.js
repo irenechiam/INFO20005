075717a2129bacdb06e5992b5aa55b8ec76d7e2c
CY <pchinyun@gmail.com>
2024-05-21 22:34:22 +1000

    update arrows and navigator html css

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-/* const slider = document.querySelector('.slider'); 
 
-const leftArrow = document.querySelector('.arrow_left');
-const rightArrow = document.querySelector('.arrow_right');
-
-let sectionIndex = 0;
-
-document.addEventListener('click',function() { 
-    if (sectionIndex < 2 ) { 
-        sectionIndex = sectionIndex + 1;  
-    } else { 
-        sectionIndex = 2
-    }
-    document.querySelector('.slider').style.transform = 'translate(' + (sectionIndex) * -33.3 + '%)';
-}); 
-
-*/ 
-
+/* 
 document.addEventListener('DOMContentLoaded', function() {
     const slider = document.querySelector('.slider');
     const leftArrow = document.querySelector('.arrow_left');
@@ -23,6 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let sectionIndex = 0;
     const totalSections = 3; // assuming there are 3 sections
+
+    document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
+        indicator.addEventListener('click', function()) { 
+            slider.style.transform = 'translate(' + (ind) * -33.3 + '%)'
+        }
+    })
 
     rightArrow.addEventListener('click', function() {
         if (sectionIndex < totalSections - 1) {
@@ -37,4 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         slider.style.transform = 'translate(' + (sectionIndex) * -33.3 + '%)';
     });
-});
+}); 
+*/ 
+
+
