afa065de9504e942c9484ae7f3ad4c8fd5e892b1
CY <pchinyun@gmail.com>
2024-05-23 13:38:06 +1000

    added product title

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-
-/* 
-document.addEventListener('DOMContentLoaded', function() {
-    const slider = document.querySelector('.slider');
-    const leftArrow = document.querySelector('.arrow_left');
-    const rightArrow = document.querySelector('.arrow_right');
-
-    let sectionIndex = 0;
-    const totalSections = 3; // assuming there are 3 sections
-
-    document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
-        indicator.addEventListener('click', function()) { 
-            slider.style.transform = 'translate(' + (ind) * -33.3 + '%)'
-        }
-    })
-
-    rightArrow.addEventListener('click', function() {
-        if (sectionIndex < totalSections - 1) {
-            sectionIndex++;
-        }
-        slider.style.transform = 'translate(' + (sectionIndex) * -33.3 + '%)';
-    });
-
-    leftArrow.addEventListener('click', function() {
-        if (sectionIndex > 0) {
-            sectionIndex--;
-        }
-        slider.style.transform = 'translate(' + (sectionIndex) * -33.3 + '%)';
-    });
-}); 
-*/ 
-
-
+// product list js. 
 let slider = document.querySelector('.slider'); 
 let slides = Array.from(slider.children); 
 let nextButton = document.querySelector('.right_arrow');
