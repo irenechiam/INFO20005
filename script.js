88f9d6a6db9cd7275de274cde08d17f0746dc310
CY <pchinyun@gmail.com>
2024-05-31 20:28:33 +1000

    worked on add to cart function

--- script.js
+++ script.js
@@ -1,81 +1,196 @@
-// product list js. 
-let slider = document.querySelector('.slider'); 
-let slides = Array.from(slider.children); 
-let nextButton = document.querySelector('.right_arrow');
-let prevButton = document.querySelector('.left_arrow');
-let dotsNav = document.querySelector('.carousel_nav'); 
-let dots = Array.from(dotsNav.children);
-let slideWidth = slides[0].getBoundingClientRect().width; 
 
 
-function setSlidePosition(slide, index) { 
-    slide.style.left = slideWidth * index + 'px'; 
-}
 
-function moveToSlide (slider, current_slide, targetSlide) { 
-    slider.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-    current_slide.classList.remove('current_slide'); 
-    targetSlide.classList.add('current_slide'); 
-}
 
-function updateDots (currentDot, targetDot) { 
-    currentDot.classList.remove('current_slide'); 
-    targetDot.classList.add('current_slide'); 
-}
 
-function arrowVisibility (slides, prevButton, nextButton, targetIndex) { 
-    if (targetIndex === 0) { 
-        prevButton.classList.add('is_hidden'); 
-        nextButton.classList.remove('is_hidden'); 
-    } else if (targetIndex === slides.length -1) { 
-        prevButton.classList.remove('is_hidden'); 
-        nextButton.classList.add('is_hidden'); 
-    } else { 
-        prevButton.classList.remove('is_hidden'); 
-        nextButton.classList.remove('is_hidden'); 
+document.addEventListener('DOMContentLoaded', () => {
+    // Product list JS
+    const slider = document.querySelector('.slider'); 
+    if (slider) {
+        const slides = Array.from(slider.children); 
+        const nextButton = document.querySelector('.right_arrow');
+        const prevButton = document.querySelector('.left_arrow');
+        const dotsNav = document.querySelector('.carousel_nav'); 
+        const dots = Array.from(dotsNav.children);
+        const slideWidth = slides[0].getBoundingClientRect().width; 
+
+        function setSlidePosition(slide, index) { 
+            slide.style.left = slideWidth * index + 'px'; 
+        }
+
+        function moveToSlide(slider, currentSlide, targetSlide) { 
+            slider.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+            currentSlide.classList.remove('current_slide'); 
+            targetSlide.classList.add('current_slide'); 
+        }
+
+        function updateDots(currentDot, targetDot) { 
+            currentDot.classList.remove('current_slide'); 
+            targetDot.classList.add('current_slide'); 
+        }
+
+        function arrowVisibility(slides, prevButton, nextButton, targetIndex) { 
+            if (targetIndex === 0) { 
+                prevButton.classList.add('is_hidden'); 
+                nextButton.classList.remove('is_hidden'); 
+            } else if (targetIndex === slides.length - 1) { 
+                prevButton.classList.remove('is_hidden'); 
+                nextButton.classList.add('is_hidden'); 
+            } else { 
+                prevButton.classList.remove('is_hidden'); 
+                nextButton.classList.remove('is_hidden'); 
+            }
+        }
+
+        slides.forEach(setSlidePosition); 
+
+        nextButton.addEventListener('click', function() {
+            const currentSlide = slider.querySelector('.current_slide'); 
+            const nextSlide = currentSlide.nextElementSibling; 
+            const currentDot = dotsNav.querySelector('.current_slide'); 
+            const nextDot = currentDot.nextElementSibling;
+            const nextIndex = slides.findIndex(slide => slide === nextSlide); 
+
+            moveToSlide(slider, currentSlide, nextSlide); 
+            updateDots(currentDot, nextDot); 
+            arrowVisibility(slides, prevButton, nextButton, nextIndex);
+        }); 
+
+        prevButton.addEventListener('click', function() { 
+            const currentSlide = slider.querySelector('.current_slide'); 
+            const prevSlide = currentSlide.previousElementSibling; 
+            const currentDot = dotsNav.querySelector('.current_slide'); 
+            const prevDot = currentDot.previousElementSibling;
+            const prevIndex = slides.findIndex(slide => slide === prevSlide); 
+            
+            moveToSlide(slider, currentSlide, prevSlide); 
+            updateDots(currentDot, prevDot);
+            arrowVisibility(slides, prevButton, nextButton, prevIndex);
+        });
+
+        dotsNav.addEventListener('click', e => { 
+            const targetDot = e.target.closest('button'); 
+            
+            if (!targetDot) return; 
+
+            const currentSlide = slider.querySelector('.current_slide'); 
+            const currentDot = dotsNav.querySelector('.current_slide'); 
+            const targetIndex = dots.findIndex(dot => dot === targetDot);
+            const targetSlide = slides[targetIndex]; 
+
+            moveToSlide(slider, currentSlide, targetSlide);
+            updateDots(currentDot, targetDot); 
+            arrowVisibility(slides, prevButton, nextButton, targetIndex);
+        });
+    }
+
+    // Product page JS
+    let count = 0;
+    let amount = 1; 
+    let celtPrice = 2000;  
+
+    const cart = document.querySelector('.cart');
+    const openCart = document.querySelector('.cart_btn');
+    const closeCart = document.querySelector('.cart_close_btn')
+
+    const totalIndicator = document.querySelector('.total_indicator'); 
+
+    const addTrigger = document.querySelector('.add_to_cart');
+    const amountToAdd = document.querySelector('.amount_to_add'); 
+    const increaseAdd = document.querySelector('.increase'); 
+    const decreaseAdd = document.querySelector('.decrease');
+
+
+    const decreaseValue = document.querySelector('.decrease_value');
+    const increaseValue = document.querySelector('.increase_value');
+    const quantityValue = document.querySelector('.quantity_value');
+    const totalSumElement = document.querySelector('.total_price');
+    
+
+    
+    function updateTotalPrice() {
+        const totalPrice = count * celtPrice;
+        totalSumElement.innerHTML = `$${totalPrice}`;
+        updateTotalIndicator();
     }
     
-}
-
-
-slides.forEach(setSlidePosition); 
-
-nextButton.addEventListener('click', function() {
-    let currentSlide = slider.querySelector('.current_slide'); 
-    let nextSlide = currentSlide.nextElementSibling; 
-    let currentDot = dotsNav.querySelector('.current_slide'); 
-    let nextDot = currentDot.nextElementSibling;
-    let nextIndex = slides.findIndex(slide => slide === nextSlide); 
-
-    moveToSlide(slider, currentSlide, nextSlide); 
-    updateDots(currentDot, nextDot); 
-    arrowVisibility (slides, prevButton, nextButton, nextIndex);
-}); 
-
-prevButton.addEventListener('click', function() { 
-    let currentSlide = slider.querySelector('.current_slide'); 
-    let prevSlide = currentSlide.previousElementSibling; 
-    let currentDot = dotsNav.querySelector('.current_slide'); 
-    let prevDot = currentDot.previousElementSibling;
-    let prevIndex = slides.findIndex(slide => slide === prevSlide); 
+    function increaseCount() {
+        count += 1; 
+        quantityValue.innerHTML = count;
+        updateTotalPrice();
+    }
     
-    moveToSlide(slider, currentSlide, prevSlide); 
-    updateDots(currentDot, prevDot);
-    arrowVisibility (slides, prevButton, nextButton, prevIndex);
-})
+    function increaseAmount() { 
+        amount += 1; 
+        amountToAdd.innerHTML = amount; 
+    }
 
-dotsNav.addEventListener('click', e => { 
-    let targetDot = e.target.closest('button'); 
+    function updateTotalIndicator() {
+       if (count > 0) { 
+        totalIndicator.innerHTML = count; 
+       } else { 
+        totalIndicator.innerHTML = 0;
+       }
+    }
+
+   
+    //within product info page 
+
+    openCart.addEventListener('click', e => { 
+        cart.style.width = '500px'; 
+    })
+
+    closeCart.addEventListener('click', e=> { 
+        cart.style.width = '0px'; 
+    })
+
+  addTrigger.addEventListener('click', e => { 
+        if (amount > 1) { 
+            count += amount; 
+            quantityValue.innerHTML = count; 
+            amount = 1; 
+            amountToAdd.innerHTML = amount; 
+        } else { 
+            increaseCount();
+        } 
+
+        updateTotalPrice();
+        cart.style.width = '500px'; 
+
+    });
+
+    increaseAdd.addEventListener('click', e => { 
+        increaseAmount(); 
+    })
+
+    decreaseAdd.addEventListener('click', e=> { 
+        if (amount > 1 ) { 
+            amount -= 1; 
+            amountToAdd.innerHTML = amount; 
+        }
+    })
+
+    // within cart
+    decreaseValue.addEventListener('click', e => { 
+        if (count > 0) {
+            count -= 1;
+            quantityValue.innerHTML = count;
+            updateTotalPrice();
+        }
+    });
     
-    if (!targetDot) return; 
+    increaseValue.addEventListener('click', e => { 
+        increaseCount();
+    });
+    
+  
+    
+    // Initial total price update
+    updateTotalPrice();
+    updateTotalIndicator();
 
-    let currentSlide = slider.querySelector('.current_slide'); 
-    let currentDot = dotsNav.querySelector('.current_slide'); 
-    let targetIndex = dots.findIndex(dot => dot === targetDot);
-    let targetSlide = slides[targetIndex]; 
+ 
 
-    moveToSlide(slider, currentSlide, targetSlide);
-    updateDots(currentDot, targetDot); 
-    arrowVisibility (slides, prevButton, nextButton, targetIndex);
+});
 
-})
+
